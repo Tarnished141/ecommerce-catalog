04f28d5af74b415b5ee32fc4bd3139c80e09b49b
Sultan Ridho <edenassasin@gmail.com>
2023-10-29 23:54:23 +0700

    commit
    first

---
+++ client/app.vue
@@ -0,0 +1,394 @@
+<template>
+  <div class="div">
+    <div class="div-2">
+      <div class="div-3">
+        <div class="div-4">
+          <div class="div-5">
+            <div class="column">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2d065636-756b-40f6-9514-2879693294db?"
+                class="img"
+              />
+            </div>
+            <div class="column-2">
+              <div class="div-6">
+                <div class="div-7">
+                  Lock and Love Women's Removable Hooded Faux Leather Moto Biker
+                  Jacket
+                </div>
+                <div class="div-8">
+                  <div class="div-9">women's clothing</div>
+                  <div class="div-10">
+                    <div class="div-11">2.9/5</div>
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/533550b3-806b-4adf-904d-96e338abafa5?"
+                      class="img-2"
+                    /><img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/e9ad84ab-14b0-49f7-8ac9-f94df8efa2c1?"
+                      class="img-3"
+                    /><img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/e73ef3f6-326b-47a6-ac77-f18a471b082f?"
+                      class="img-4"
+                    /><img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/c6dd48fa-495d-4d88-a331-63a94d88669c?"
+                      class="img-5"
+                    /><img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/003dee4b-ca1d-4af9-ab2e-47e3c3b1c225?"
+                      class="img-6"
+                    />
+                  </div>
+                </div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/3774e1a4-8402-4981-95d7-63257881026c?"
+                  class="img-7"
+                />
+                <div class="div-12">
+                  100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER
+                  25% COTTON (SWEATER), Faux leather material for style and
+                  comfort / 2 pockets of front, 2-For-One Hooded denim style
+                  faux leather jacket, Button detail on waist / Detail stitching
+                  at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT
+                  IRON
+                </div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/98409cf3-e7b2-44f0-94b5-1a0c7b175e2b?"
+                  class="img-8"
+                />
+                <div class="div-13">$29.95</div>
+                <div class="div-14">
+                  <div class="div-15"><div class="div-16">Buy now</div></div>
+                  <div class="div-17">
+                    <div class="div-18">Next product</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+.div {
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+}
+.div-2 {
+  background-color: #fde2ff;
+  align-self: stretch;
+  display: flex;
+  margin-bottom: 227px;
+  width: 100%;
+  flex-direction: column;
+  padding: 126px 20px -158px;
+}
+@media (max-width: 991px) {
+  .div-2 {
+    max-width: 100%;
+    margin-bottom: 40px;
+  }
+}
+.div-3 {
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background-color: #fff;
+  align-self: center;
+  z-index: 1;
+  display: flex;
+  width: 100%;
+  max-width: 1034px;
+  flex-direction: column;
+  padding: 57px 20px 46px;
+}
+@media (max-width: 991px) {
+  .div-3 {
+    max-width: 100%;
+  }
+}
+.div-4 {
+  align-self: center;
+  width: 922px;
+  max-width: 100%;
+}
+.div-5 {
+  gap: 20px;
+  display: flex;
+}
+@media (max-width: 991px) {
+  .div-5 {
+    flex-direction: column;
+    align-items: stretch;
+    gap: 0px;
+  }
+}
+.column {
+  display: flex;
+  flex-direction: column;
+  line-height: normal;
+  width: 36%;
+  margin-left: 0px;
+}
+@media (max-width: 991px) {
+  .column {
+    width: 100%;
+  }
+}
+.img {
+  aspect-ratio: 0.75;
+  object-fit: cover;
+  object-position: center;
+  width: 306px;
+  fill: url(<path-to-image>), lightgray 50% / cover no-repeat;
+  overflow: hidden;
+  margin-top: 16px;
+  max-width: 100%;
+}
+@media (max-width: 991px) {
+  .img {
+    margin-top: 40px;
+  }
+}
+.column-2 {
+  display: flex;
+  flex-direction: column;
+  line-height: normal;
+  width: 64%;
+  margin-left: 20px;
+}
+@media (max-width: 991px) {
+  .column-2 {
+    width: 100%;
+  }
+}
+.div-6 {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+}
+@media (max-width: 991px) {
+  .div-6 {
+    max-width: 100%;
+    margin-top: 40px;
+  }
+}
+.div-7 {
+  color: #720060;
+  font: 600 28px Inter, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-7 {
+    max-width: 100%;
+  }
+}
+.div-8 {
+  align-self: start;
+  display: flex;
+  margin-top: 34px;
+  width: 537px;
+  max-width: 100%;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 20px;
+}
+@media (max-width: 991px) {
+  .div-8 {
+    flex-wrap: wrap;
+  }
+}
+.div-9 {
+  color: #3f3f3f;
+  flex: 1;
+  font: 400 18px Inter, sans-serif;
+}
+.div-10 {
+  align-self: start;
+  display: flex;
+  align-items: flex-start;
+  gap: 2px;
+}
+@media (max-width: 991px) {
+  .div-10 {
+    justify-content: center;
+  }
+}
+.div-11 {
+  color: #3f3f3f;
+  align-self: start;
+  font: 400 18px Inter, sans-serif;
+}
+.img-2 {
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  width: 18px;
+  overflow: hidden;
+  border-radius: 50%;
+  align-self: start;
+  max-width: 100%;
+}
+.img-3 {
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  width: 18px;
+  overflow: hidden;
+  border-radius: 50%;
+  align-self: start;
+  max-width: 100%;
+}
+.img-4 {
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  width: 18px;
+  overflow: hidden;
+  border-radius: 50%;
+  align-self: start;
+  max-width: 100%;
+}
+.img-5 {
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  width: 18px;
+  stroke-width: 1px;
+  overflow: hidden;
+  border-radius: 50%;
+  align-self: start;
+  max-width: 100%;
+}
+.img-6 {
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  width: 18px;
+  stroke-width: 1px;
+  overflow: hidden;
+  border-radius: 50%;
+  align-self: start;
+  max-width: 100%;
+}
+.img-7 {
+  aspect-ratio: 543;
+  object-fit: cover;
+  object-position: center;
+  width: 543px;
+  stroke-width: 1px;
+  stroke: rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  align-self: start;
+  margin-top: 11px;
+  max-width: 100%;
+}
+.div-12 {
+  color: #1e1e1e;
+  align-self: start;
+  margin-top: 29px;
+  max-width: 517px;
+  font: 400 20px Inter, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-12 {
+    max-width: 100%;
+  }
+}
+.img-8 {
+  aspect-ratio: 543;
+  object-fit: cover;
+  object-position: center;
+  width: 543px;
+  stroke-width: 1px;
+  stroke: rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  align-self: start;
+  margin-top: 60px;
+  max-width: 100%;
+}
+@media (max-width: 991px) {
+  .img-8 {
+    margin-top: 40px;
+  }
+}
+.div-13 {
+  color: #720060;
+  align-self: start;
+  margin-top: 19px;
+  text-wrap: nowrap;
+  font: 600 28px Inter, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-13 {
+    text-wrap: initial;
+  }
+}
+.div-14 {
+  align-self: start;
+  display: flex;
+  margin-top: 19px;
+  width: 537px;
+  max-width: 100%;
+  align-items: flex-start;
+  gap: 19px;
+}
+@media (max-width: 991px) {
+  .div-14 {
+    flex-wrap: wrap;
+  }
+}
+.div-15 {
+  border-radius: 4px;
+  background-color: #720060;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-basis: auto;
+  flex: 1;
+  padding: 12px 20px;
+}
+.div-16 {
+  color: #fff;
+  align-self: center;
+  text-wrap: nowrap;
+  font: 600 20px Inter, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-16 {
+    text-wrap: initial;
+  }
+}
+.div-17 {
+  border-radius: 4px;
+  border: 3px solid #720060;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-basis: auto;
+  flex: 1;
+  padding: 12px 20px;
+}
+.div-18 {
+  color: #720060;
+  align-self: center;
+  text-wrap: nowrap;
+  font: 600 20px Inter, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-18 {
+    text-wrap: initial;
+  }
+}
+</style>
